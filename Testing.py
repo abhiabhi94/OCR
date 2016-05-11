28c91e0374c4908e9a414a05ca9cfaa99b7970db
Anupam Singh <anupamsingh610@gmail.com>
2016-05-11 13:26:52 +0530

    Space and Line Change Optimized

--- Testing.py
+++ Testing.py
@@ -4,7 +4,8 @@ import cv2, operator, os, numpy as np
 MIN_CONTOUR_AREA = 50
 RESIZED_IMAGE_WIDTH = 20
 RESIZED_IMAGE_HEIGHT = 20
-PATH = "test 2.jpg"
+PATH = "test C1.jpg"
+DIVISION_FACTOR = 600
 
 allContoursWithData = []
 validContoursWithData = []
@@ -25,6 +26,8 @@ class ContourWithData():
         self.intRectHeight = 0           # bounding rect height
         self.fltArea = 0.0               # area of contour
         self.aspectRatio = 0.0
+        self.XCentroid = 0.0
+        self.YCentroid = 0.0
 
     def rectDetails(self):               # calculate bounding rect info
         [intX, intY, intWidth, intHeight] = self.boundingRect
@@ -33,6 +36,8 @@ class ContourWithData():
         self.intRectWidth = intWidth
         self.intRectHeight = intHeight
         self.aspectRatio = float(intWidth) / intHeight
+        self.XCentroid = intX + intWidth/2
+        self.YCentroid = intY + intHeight/2
 
     def contourCheck(self):
         if self.fltArea > MIN_CONTOUR_AREA and (0.5 < self.aspectRatio < 2): return True    # much better validity checking would be necessary  
@@ -68,24 +73,58 @@ def getContourDetails(npaContours):
 
     return allContoursWithData
 
+# def compare(contourWithData1,contourWithData2):
+#     if(contourWithData1.intRectY - contourWithData2.intRectY < 10):
+#         if(contourWithData1.intRectX < contourWithData2.intRectX):
+#             return -1
+#         elif(contourWithData1.intRectX > contourWithData2.intRectX):
+#             return 1
+#         else:
+#             return 0
+#     else:
+#         if(contourWithData1.intRectY < contourWithData2.intRectY):
+#             return -1
+#         elif(contourWithData1.intRectY > contourWithData2.intRectY):
+#             return 1
+#         else:
+#             return 0
+
+def compare(contourWithData1,contourWithData2):
+    if(contourWithData1.intRectY - contourWithData2.intRectY < 10):
+        if(contourWithData1.intRectX < contourWithData2.intRectX):
+            return -1
+        elif(contourWithData1.intRectX > contourWithData2.intRectX):
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+
 def getValidContours(allContoursWithData):
 
     for contourWithData in allContoursWithData:
         if contourWithData.contourCheck():
             validContoursWithData.append(contourWithData)
 
-    validContoursWithData.sort(key = operator.attrgetter("intRectX"))
+    validContoursWithData.sort(key = operator.attrgetter("intRectY"))
+    validContoursWithData.sort(cmp = compare)
+    # validContoursWithData.sort(cmp = compare)
     return validContoursWithData
 
-def spaceCheck(contourWithData, i, length): 
-        currentCentroid = contourWithData.intRectX + contourWithData.intRectWidth / 2
+def formattingCheck(contourWithData, i, length): 
         if (i != length):
-            nextCentroid = validContoursWithData[i].intRectX + validContoursWithData[i].intRectWidth / 2
+            nextContour = validContoursWithData[i]
         else:
-            nextCentroid = validContoursWithData[i-1].intRectX + validContoursWithData[i-1].intRectWidth / 2
+            nextContour = validContoursWithData[i-1]
             # print strFinalString
-        if (nextCentroid - currentCentroid > 30): return True     #Much better Check required
-        return False
+        if(nextContour.YCentroid - contourWithData.YCentroid > 30):
+            line_change = "\n"
+        else:
+            line_change = ""
+        if (nextContour.XCentroid - contourWithData.XCentroid> 40):
+            return " ",line_change     #Much better Check required
+        return "",line_change
+
     
 def OCR(img, imgThresh, validContoursWithData):
     
@@ -94,10 +133,13 @@ def OCR(img, imgThresh, validContoursWithData):
     i = 0
     a = 0.0
     length = len(validContoursWithData)
-
+    print length
     for contourWithData in validContoursWithData:
         i += 1
+        # if(i > 9 ):
         cv2.rectangle(img, (contourWithData.intRectX, contourWithData.intRectY),(contourWithData.intRectX + contourWithData.intRectWidth, contourWithData.intRectY + contourWithData.intRectHeight),(0, 255, 0),2)
+        # if(i == 45):
+        # cv2.putText(img, str(i), (contourWithData.XCentroid, contourWithData.YCentroid),  cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         imgROI = imgThresh[contourWithData.intRectY : contourWithData.intRectY + contourWithData.intRectHeight,contourWithData.intRectX : contourWithData.intRectX + contourWithData.intRectWidth]
         imgROIResized = cv2.resize(imgROI, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))
         npaROIResized = imgROIResized.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))
@@ -109,10 +151,9 @@ def OCR(img, imgThresh, validContoursWithData):
         #npaResults = svm.predict_all(npaROIResized)
         strCurrentChar = chr(int(npaResults[0][0]))
         
-        if (spaceCheck(contourWithData, i, length)):
-            strFinalString = strFinalString + strCurrentChar + " "
-        else:
-            strFinalString = strFinalString + strCurrentChar
+        space,line_change = formattingCheck(contourWithData, i, length)
+        strFinalString = strFinalString + strCurrentChar + space + line_change
+
 
         # cv2.namedWindow('Fuck '+str(i), cv2.WINDOW_NORMAL)
         # cv2.imshow('Fuck '+str(i), imgROI)
@@ -129,7 +170,7 @@ def OCR(img, imgThresh, validContoursWithData):
 def main():
 
     img = cv2.imread(PATH)
-    RP = img.shape[0]/ 400 if img.shape[0] <= img.shape[1] else img.shape[1]/ 400
+    RP = img.shape[0]/ DIVISION_FACTOR if img.shape[0] <= img.shape[1] else img.shape[1]/ DIVISION_FACTOR
     img = cv2.resize (img, (img.shape[1] / RP, img.shape[0] / RP))
     #cv2.imshow('imgTestingNumber1',img)
     imgThreshCopy, imgThresh = preprocess(img)
@@ -144,7 +185,7 @@ def main():
     OCR(img, imgThresh, validContoursWithData)
     #print "\n" + strFinalString + "\n"
 
-    cv2.imshow("img", img)
+    cv2.imshow("img.jpg", img)
     # cv2.imwrite("photo_ocr_example.jpg",img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
